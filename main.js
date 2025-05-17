da2d99ba925b304d420d7f98d187ec6e53112e1b
tarik pinarli <tarikpinarli@tariks-MacBook-Air.local>
2025-05-17 20:39:39 +0300

    almost there

--- main.js
+++ main.js
@@ -65,3 +65,8 @@ async function askGuide(userQuestion) {
     console.error("Fetch error:", err);
   }
 }
+function speak(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'en-US';
+  speechSynthesis.speak(utterance);
+}
